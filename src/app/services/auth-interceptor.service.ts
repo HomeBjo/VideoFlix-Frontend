4ff85b1d419d40ed541855242157f5f2534bedd3
HomeBjo <lala2-wuppertal@hotmail.com>
2024-08-06 14:58:28 +0200

    add environments

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -19,8 +19,8 @@ export class AuthInterceptorService implements HttpInterceptor {
           Authorization: `Token ${authToken}`
         }
       });
-    } else {
-      console.log('No token found'); 
+     } else {
+      console.log('No token found');
     }
 
     return next.handle(req).pipe(
