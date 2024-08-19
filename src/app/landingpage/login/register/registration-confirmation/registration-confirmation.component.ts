dcd3728631932bb8a680d4e65d021098e0e124a8
HomeBjo <lala2-wuppertal@hotmail.com>
2024-08-19 09:23:36 +0200

    shared header/footer

--- src/app/landingpage/login/register/registration-confirmation/registration-confirmation.component.ts
+++ src/app/landingpage/login/register/registration-confirmation/registration-confirmation.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../../services/user-service.service';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../../../../shared/login/footer/footer.component";
+import { HeaderComponent } from "../../../../shared/login/header/header.component";
 
 @Component({
   selector: 'app-registration-confirmation',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink],
+  imports: [FormsModule, CommonModule, RouterLink, FooterComponent, HeaderComponent],
   templateUrl: './registration-confirmation.component.html',
   styleUrl: './registration-confirmation.component.scss'
 })
