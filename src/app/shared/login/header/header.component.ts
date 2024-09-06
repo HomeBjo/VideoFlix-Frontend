9acba7c5bb70a8da8fe1c80c1fe2671b426d7b5e
HomeBjo <lala2-wuppertal@hotmail.com>
2024-09-06 08:33:22 +0200

    login header show funktion

--- src/app/shared/login/header/header.component.ts
+++ src/app/shared/login/header/header.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  @Input() showImage: boolean = true;
 }
