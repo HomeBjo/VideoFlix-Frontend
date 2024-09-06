ceeb7da4a19c91cb74a8d2a198d79b076af1338b
HomeBjo <lala2-wuppertal@hotmail.com>
2024-09-06 10:46:52 +0200

    landingpage responsiv

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../services/user-service.service';
+import { HeaderComponent } from "../shared/login/header/header.component";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink],
+  imports: [FormsModule, CommonModule, RouterLink, HeaderComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
