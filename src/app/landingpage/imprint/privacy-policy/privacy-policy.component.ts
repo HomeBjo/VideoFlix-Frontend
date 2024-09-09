53223cd9e105386741f567728227555867b652a9
HomeBjo <lala2-wuppertal@hotmail.com>
2024-09-09 16:25:30 +0200

    add imprint/privacy

--- src/app/landingpage/imprint/privacy-policy/privacy-policy.component.ts
+++ src/app/landingpage/imprint/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
