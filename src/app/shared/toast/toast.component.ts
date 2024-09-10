33b6ead052b2fdfdf00d5885ede36dec7e1fea2a
HomeBjo <lala2-wuppertal@hotmail.com>
2024-09-10 14:19:22 +0200

    cleancode sharedFolder

--- src/app/shared/toast/toast.component.ts
+++ src/app/shared/toast/toast.component.ts
@@ -1,40 +1,32 @@
-import { Component,Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './toast.component.html',
-  styleUrl: './toast.component.scss'
+  styleUrl: './toast.component.scss',
 })
 export class ToastComponent {
   @Input() message: string = '';
-  @Input() type: 'success' | 'error' | 'info' = 'info'; // Typen können angepasst werden
+  @Input() type: 'success' | 'error' | 'info' = 'info';
   show: boolean = false;
   hide: boolean = false;
 
   showToast() {
-    this.show = true;  // zur sicherheit booleans setzen !
-    this.hide = false; 
-  
-    // Starte die SlideOut nach 5 sek
+    this.show = true;
+    this.hide = false;
+
     setTimeout(() => {
       this.hide = true;
-  
-      // Entferne show klasse nach der animation
+
       setTimeout(() => {
         this.show = false;
-      }, 500); //  Verzögerung für slideout
-    }, 5000); // zeit wan slideout benutzt wird
+      }, 500);
+    }, 5000);
   }
-
-  // showToast() {
-  //   console.log('Toast wird angezeigt'); 
-  //   this.show = true;
-  // // zum css stylen ----------------------------
-  // }
 
   closeToast() {
     this.show = false;
--- src/app/shared/videoSite/header/header.component.ts
+++ src/app/shared/videoSite/header/header.component.ts
@@ -1,65 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { UserService } from '../../../services/user-service.service';
-import { VideoService } from '../../../services/video-service.service';
-import { RouterLink,ActivatedRoute } from '@angular/router';
-
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  constructor(public userService: UserService, private videoService: VideoService, private route: ActivatedRoute) {}
-  shwonProfilSelection:boolean = false;
-  showCategorySelection:boolean = false;
-  showMobileSelection:boolean = false;
+  constructor(
+    public userService: UserService
+  ) {}
+  shwonProfilSelection: boolean = false;
+  showCategorySelection: boolean = false;
+  showMobileSelection: boolean = false;
 
-    // toggle anzeige 
-    toggleCategoryMenu(event: MouseEvent) {
-      event.stopPropagation();
-      this.showCategorySelection = !this.showCategorySelection;
+  toggleCategoryMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.showCategorySelection = !this.showCategorySelection;
+  }
+  toggleMobileMenu() {
+    this.showMobileSelection = !this.showMobileSelection;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleOutsideClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+   
+    if (
+      !target.closest('.mobileSection') &&
+      !target.closest('.burgerMenuIcon')
+    ) {
+      this.showMobileSelection = false;
     }
-    toggleMobileMenu(){
-      this.showMobileSelection = !this.showMobileSelection;
-    }
-  
-  // prüfe den klick auserhalb der div
-    // @HostListener('document:click', ['$event'])
-    // onClick(event: MouseEvent) {
-    //   this.showCategorySelection = false;
-    //   this.shwonProfilSelection = false;
-    // }
     
-    @HostListener('document:click', ['$event'])
-    handleOutsideClick(event: MouseEvent) {
-      const target = event.target as HTMLElement;
-      // Schließe das mobile Menü, wenn außerhalb des Menüs geklickt wird
-      if (!target.closest('.mobileSection') && !target.closest('.burgerMenuIcon')) {
-        this.showMobileSelection = false;
-      }
-      // Schließe das Kategorie-Menü, wenn außerhalb geklickt wird
-      if (!target.closest('.categorySelection') && !target.closest('.headerBtn')) {
-        this.showCategorySelection = false;
-      }
-      if(!target.closest('slideInAnimation')&& !target.closest('slideOutAnimation')){
-        this.shwonProfilSelection = false;
-      }
+    if (
+      !target.closest('.categorySelection') &&
+      !target.closest('.headerBtn')
+    ) {
+      this.showCategorySelection = false;
     }
+    if (
+      !target.closest('slideInAnimation') &&
+      !target.closest('slideOutAnimation')
+    ) {
+      this.shwonProfilSelection = false;
+    }
+  }
 
-    logout(){
-      localStorage.setItem('logoutInProgress', 'true');
-      let userID = localStorage.getItem('userId')?.toString();
-      this.userService.userLogout(userID!);
-    }
-    openSmallMenu(event: MouseEvent){
-      event.stopPropagation();
-      this.shwonProfilSelection = !this.shwonProfilSelection;
-    }
-  
-
+  logout() {
+    localStorage.setItem('logoutInProgress', 'true');
+    let userID = localStorage.getItem('userId')?.toString();
+    this.userService.userLogout(userID!);
+  }
+  openSmallMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.shwonProfilSelection = !this.shwonProfilSelection;
+  }
 }
